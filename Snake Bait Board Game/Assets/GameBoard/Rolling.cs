c99a698544b74d67fa641860aea75d71673a8de6
Woodwing <woodwing@Woodwings-MacBook-Air.local>
2018-04-11 12:53:56 -0400

    revert

---
+++ Snake Bait Board Game/Assets/GameBoard/Rolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DiceValues = new int[2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int[] DiceValues;
+	public Sprite[] DiceOne;
+	public Sprite[] DiceTwo;
+
+    public void RollTheDice()
+    {
+        //you roll two dice
+        DiceValues[0] = Random.Range(1, 7);
+		DiceValues[1] = Random.Range(1, 7);
+
+        Debug.Log("Rolled a " + DiceValues[0] + " and a " + DiceValues[1]);
+
+
+
+    }
+}
